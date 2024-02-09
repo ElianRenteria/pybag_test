8240a59d133c74480afec746cb3d0fc6cabee2d5
ElianRenteria <elianrenteriadevelopment@gmail.com>
2024-02-09 15:27:43 -0800

    pygbag files added

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+import asyncio
 from player import Player
 from animations import bg
 from particle import Particle
 
 root = pygame.display.set_mode((1920, 930))
 pygame.display.set_caption("Game3")
+player = Player()
+parties = pygame.sprite.Group()
+clock = pygame.time.Clock()
 def draw():
     root.blit(bg, (0, 0))
     if player.alive:
@@ -23,14 +27,15 @@ def draw():
         player.update(root)
 
 
-player = Player()
-parties = pygame.sprite.Group()
-clock = pygame.time.Clock()
-while True:
-    clock.tick(30)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    draw()
-    pygame.display.update()
+async def main():
+    while True:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        draw()
+        pygame.display.update()
+        await asyncio.sleep(0)
+
+asyncio.run(main())
